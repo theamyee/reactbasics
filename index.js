500c233ab654d6961546a4c4e66420eed5a768ef
theamyee <111275471+theamyee@users.noreply.github.com>
2022-08-24 16:09:05 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";   //imr
+import ReactDOM from 'react-dom';   //imrd
+import RootComponent from "./rootcomponent/root";
+
+ReactDOM.render(<RootComponent></RootComponent>,document.getElementById("root"))
